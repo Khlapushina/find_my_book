4509e18382b644739c1636685c752a6706d3a8cc
Norgan97 <daniil.lvov.a97@gmail.com>
2023-11-10 16:46:51 +0300

    final

--- README.md
+++ README.md
@@ -1,1 +1,56 @@
-# find_my_book
+---
+title: Find My Book
+emoji: 🌍
+colorFrom: red
+colorTo: purple
+sdk: streamlit
+sdk_version: 1.28.1
+app_file: app.py
+pinned: false
+license: apache-2.0
+---
+
+Check out the configuration reference at https://huggingface.co/docs/hub/spaces-config-reference
+
+
+
+# Recommendation system project
+# Find my book  
+![](https://sun9-46.userapi.com/impg/HvvkmK5Z3-HWHh2eT7Knv-E-ibwK04HI25ASUg/aBMfuCXNiR0.jpg?size=1920x1285&quality=95&sign=48abf3e3dc9344005305f55f18ddee5d&type=album)
+### Additional subtask:
+# Find a dish for me
+![](https://i.imgur.com/LqccfIw.jpg)
+
+# Elbrus Bootcamp | Phase-2 | Team Project
+
+## Team
+* [Daniil Lvov](https://huggingface.co/Norgan97)
+* [Dmitry Budazhapov](https://huggingface.co/DmitryDorzhievich)
+* [Larisa Khlapushina](https://huggingface.co/Larrisa)
+___
+## Tasks
+In this project, our team has developed a book search system based on user requests. 
+The service takes a user's description of the book as input and returns a specified number of suitable options. 
+In addition to book search, we can offer a system that will find the desired dish and a link to its recipe.
+In this case, the user only needs to enter an approximate description of the characteristics and ingredients.
+We have implemented a mechanism for analyzing and comparing textual descriptions of dishes, 
+which allows for effectively finding alternatives and variations of desired dishes. 
+The work represents an innovative approach to searching for culinary recipes and can be useful for both professional chefs and culinary enthusiasts.
+___
+## Contents
+1. Parsing information from websites.
+2. Vectorization using a model *rubert-tiny2*
+3. Finding similar vectors using *faiss*.
+___
+## Deployment
+The service is implemented on [Streamlit](https://huggingface.co/spaces/ds-meteors/find_my_book)
+_
+## How to run locally?
+## To run the provided applications on your computer, follow these steps:
+
+1. Clone this repository to your local machine.
+2. Install the required libraries by running the command *pip install -r requirements.txt* in your terminal or command prompt.
+3. Once the libraries are installed, navigate to the repository's directory in your terminal.
+4. Run the command *streamlit run main.py* in your terminal to start the application.
+
+This will launch the Streamlit server, and you can access the applications by opening a browser window and navigating to the specified URL.
---
+++ app.py
@@ -0,0 +1,85 @@
+import streamlit as st
+import pandas as pd 
+import torch
+from PIL import Image
+from io import BytesIO
+import requests
+import faiss
+
+
+from transformers import AutoTokenizer, AutoModel
+import numpy as np
+st.set_page_config(layout="wide")
+
+@st.cache_resource()
+def load_model():
+    model = AutoModel.from_pretrained("cointegrated/rubert-tiny2")
+    tokenizer = AutoTokenizer.from_pretrained("cointegrated/rubert-tiny2")
+    return model , tokenizer
+
+model, tokenizer = load_model()
+
+@st.cache_data()
+def load_data():
+    df = pd.read_csv('Dataset/parcedbooks.csv')
+    with open('Dataset/embeddingsbooks.txt', 'r') as file:
+        embeddings_list = [list(map(float, line.split())) for line in file.readlines()]
+    index = faiss.read_index('Dataset/faissbooks.index')
+    return df, embeddings_list, index
+
+df, embeddings_list, index = load_data()
+
+def embed_bert_cls(text, model, tokenizer):
+    t = tokenizer(text, padding=True, truncation=True, return_tensors='pt')
+    with torch.no_grad():
+        model_output = model(**{k: v.to(model.device) for k, v in t.items()})
+    embeddings = model_output.last_hidden_state[:, 0, :]
+    embeddings = torch.nn.functional.normalize(embeddings)
+    return embeddings[0].cpu().numpy()
+
+
+col3, col4 = st.columns([5,1])
+
+with col3:
+    text = st.text_input('Введите ваше предпочтение для рекомендации')
+with col4:
+    num = st.number_input('Количество книг', step=1, value=1)
+    button = st.button('Отправить запрос')
+
+
+if text and button:
+    decode_text = embed_bert_cls(text, model, tokenizer)  # Получение вектора для введенного текста
+    k = num 
+    D, I = index.search(decode_text.reshape(1, -1), k)
+    
+    top_similar_indices = I[0]
+    top_similar_annotations = [df['annotation'].iloc[i] for i in top_similar_indices]
+    top_similar_images = [df['image_url'].iloc[i] for i in top_similar_indices]
+    images = [Image.open(BytesIO(requests.get(url).content)) for url in top_similar_images]
+    top_similar_authors = [df['author'].iloc[i] for i in top_similar_indices]
+    top_similar_title = [df['title'].iloc[i] for i in top_similar_indices]
+    top_similar_url = [df['page_url'].iloc[i] for i in top_similar_indices]
+    top_cosine_similarities = [1 - d / 2 for d in D[0]]  # Преобразование расстояний в косинусное сходство
+
+# Отображение изображений и названий
+    for similarity, image, author, annotation, title, url in zip(top_cosine_similarities, images, top_similar_authors, top_similar_annotations, top_similar_title, top_similar_url):
+        col1, col2 = st.columns([3, 4]) 
+        with col1:
+            st.image(image, width=300)
+        with col2:
+            st.write(f"***Автор:*** {author}")
+            st.write(f"***Название:*** {title}")
+            st.write(f"***Аннотация:*** {annotation}")
+            similarity = float(similarity)
+            st.write(f"***Cosine Similarity : {round(similarity, 3)}***")
+            st.write(f"***Ссылка на книгу : {url}***")
+
+        st.markdown(
+        "<hr style='border: 2px solid #000; margin-top: 10px; margin-bottom: 10px;'>",
+        unsafe_allow_html=True
+    )
+
+
+
+
+
